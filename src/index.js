f4b60024f434d2c1cab669b3c35a80dc1f917f25
di-shykh <di49@mail.ru>
2024-08-08 21:49:07 +0300

    fix: new solution for task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-module.exports = function reverse (n) {
-  if(n<0) n=Math.abs(n);
-  return +n.toString().split("").reverse().join("");
+module.exports = function reverse(n) {
+    return Math.abs(n).toString().split('').reverse().join('');
 }
